d1a42b6916f2e92c1d357ee4838005d8f9d3a22e
kainphoton <luckyphotonkain@protonmail.com>
2022-08-29 22:08:40 +1000

    Update database model with new schema for nursing home
    Also changed the signup page

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from .models import User, NursingHome
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -8,6 +8,12 @@ from flask_login import login_user, login_required, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
 
+"""
+LOGIN PAGE NOTES:
+    Add the radiobox option whether to sign in as admin or guest? Instead of creating a separate guest account each time 
+    for each nursing home from the signup page. Though it does create some security risk if the staff member saves the 
+    account email and password to the web browser, but can assume that they don't do that? Oh well it's just a prototype anyway.
+"""
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -38,27 +44,41 @@ def logout():
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+        # Default activity and wellbeing list
+        activity_list = "bowling,tennis,golf,bike,tea,chess"
+        wellbeing_list = "flat,very-sad,happy,sick,sad,angry"
+        
+        # Creates the one admin profile for each nursing home for now
+        nursing_home_name = request.form.get('nursing-home-name')
+        email = request.form.get('email-admin')
+        password1 = request.form.get('password1-admin')
+        password2 = request.form.get('password2-admin')
+        admin = True # Can remove if using the radiobox signin feature
 
+        # Look up email and nursing home name to see if they exist in the database already
         user = User.query.filter_by(email=email).first()
+        existing_nursing_home_name = NursingHome.query.filter_by(name=nursing_home_name).first()
         if user:
             flash('Email already exists.', category='error')
+        elif existing_nursing_home_name:
+            flash('Nursing Home Name already exists.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            # Add new NursingHome
+            new_nursing_home = NursingHome(activity_list=activity_list, wellbeing_list=wellbeing_list, name=nursing_home_name)
+            db.session.add(new_nursing_home)
+            db.session.commit()
+            
+            # Add new User
+            new_user = User(admin=admin, email=email, password=generate_password_hash(password1, method='sha256'), nursing_home_id=new_nursing_home.id) 
             db.session.add(new_user)
             db.session.commit()
+            
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
--- website/models.py
+++ website/models.py
@@ -3,16 +3,46 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+############################ Previous Schemas Placeholder ############################
+# class Note(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     data = db.Column(db.String(10000))
+#     date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(150), unique=True)
+#     password = db.Column(db.String(150))
+#     first_name = db.Column(db.String(150))
+#     notes = db.relationship('Note')
+
+############################ New Schemas ############################
+class NursingHome(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
+    activity_list = db.Column(db.String(10000))     # In CSV format, e.g -> "bowling,tennis,golf,bike,tea,chess"
+    wellbeing_list = db.Column(db.String(10000))    # In CSV format, e.g -> "flat,very-sad,happy,sick,sad,angry"
+    name = db.Column(db.String(10000), unique=True)
+    users = db.relationship('User')
+    
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    admin = db.Column(db.Boolean)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    nursing_home_id = db.Column(db.Integer, db.ForeignKey(NursingHome.id))
+    
+# Could probably combine the activity and the wellbeing schema together and have a text column that says one of the following: activity, wellbeing, food
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    name = db.Column(db.String(150), unique=True)
+    nursing_home_id = db.Column(db.Integer, db.ForeignKey(NursingHome.id))
+    
+class Wellbeing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    name = db.Column(db.String(150), unique=True)
+    nursing_home_id = db.Column(db.Integer, db.ForeignKey(NursingHome.id))
+    
